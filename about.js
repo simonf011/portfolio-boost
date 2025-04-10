1e14828d96a524078b6e1dca240f21d64e62b5fc
Phan Thi Mai <marittedphyed911@hotmail.com>
2025-04-10 09:02:20 +0700

    Create about.js

---
+++ about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="text-gray-700 space-y-4">
+      <img src="/profile.jpg" alt="Profile" className="w-32 h-32 rounded-full mx-auto" />
+      <p>
+        I am a web developer passionate about building dynamic, user-friendly applications.
+      </p>
+      <p>
+        I enjoy learning new technologies and am always looking to improve my skillset.
+      </p>
+    </div>
+  );
+};
+
+export default About;
