e799eac67173ca8cc2bacaa5e25187e542707ee6
Phan Thi Mai <marittedphyed911@hotmail.com>
2025-04-10 09:02:10 +0700

    Create skills.js

---
+++ skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <ul className="grid gap-2">
+      <li className="bg-gray-100 rounded p-2">HTML</li>
+      <li className="bg-gray-100 rounded p-2">CSS</li>
+      <li className="bg-gray-100 rounded p-2">JavaScript</li>
+      <li className="bg-gray-100 rounded p-2">React</li>
+      <li className="bg-gray-100 rounded p-2">Node.js</li>
+    </ul>
+  );
+};
+
+export default Skills;
